a270901dd7cd233e1e42d5807648dceaeb90c098
qetqet910 <79036088+qetqet910@users.noreply.github.com>
2024-01-14 03:55:46 +0900

    Sec

--- FE/JS/script.js
+++
@@ -1,3 +0,0 @@
-document.write("<script type='module' src='JS/components/page.js' defer></script>");
-document.write("<script type='module' src='JS/components/animation.js' defer></script>");
-document.write("<script type='module' src='JS/components/Survey.js'></script>");
